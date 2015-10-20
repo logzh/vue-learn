8265552234da7ae1c3c95a4474b130492a361f86
Spence <zjhhust@163.com>
2015-10-20 18:13:14 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,22 +7,20 @@ var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
 
 module.exports = {
     entry: {
-        thread: './public/js/bbs/thread.js',
-        demo: './public/js/demo.js'
+        thread: './public/js/bbs/thread.js',//页面1或者模块1入口
+        demo: './public/js/demo.js'//页面2或者模块2入口
     },
     output: {
-        path: path.join(process.cwd(), 'public/build/'),
-        filename: '[name].bundle.js',
-        publicPath:'build/'//http://mycdn/asset/
-        //sourceMapFilename: '[file].map',
-        //chunkFilename: "[chunkhash].bundle.js"
+        path: path.join(process.cwd(), 'public/build/'),//打包的目标目录
+        filename: '[name].bundle.js',     //生成的文件名
+        publicPath:'build/'      //如果资源需要上传到cdn可以使用 http://mycdn/asset/
     },
     resolve: {
-        extensions: ['', '.js', '.jsx'],
-        root:path.join(process.cwd(), 'public/js/'),
+        extensions: ['', '.js', '.jsx'],// 配置可以不书写的后缀名
+        root:path.join(process.cwd(), 'public/js/'), //配置绝对路径，alias中会使用
         alias: { zepto: "lib/zepto.js" }
     },
-    module: {
+    module: {//各种加载器
         loaders: [{
             test: /\.jsx?$/,
             exclude: /node_modules/,
@@ -35,14 +33,14 @@ module.exports = {
             loader: 'style!css'
         },{
             test: /\.(jpg|png)$/,
-            loader: 'url?limit=8192'
+            loader: 'url?limit=8192'//小于8kb的图片转化为base64，css中其他的图片地址会被体会为打包的地址，此处用到了publicPath
         }]
     },
     plugins: [
         commonsPlugin
         //,
-        //function() {
-        //    this.plugin("done", function(stats) {
+        //function() {//https://webpack.github.io/docs/long-term-caching.html
+        //    this.plugin("done", function(stats) {//生成对于的打包文件，详细信息，特别是hash信息 
         //        require("fs").writeFileSync(
         //            path.join(process.cwd(), "stats.json"),
         //            JSON.stringify(stats.toJson()));
